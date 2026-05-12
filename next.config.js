2694b2cc0bf43b6bc393d60a90f774f136a7d1dd
wholehousecs <wholehousecs@gmail.com>
2026-05-12 06:32:43 -0400

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
